2ddf5d842fc5264c2282614014a388880c8eb207
kayanerocha <kayannerch@gmail.com>
2024-08-17 19:44:10 -0300

    feat: registrando blueprint de usuario, migration de tipo_usuario e populando os tipos de usuários

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+from distutils.log import debug
 from decouple import config
 from flask import Flask
 from flask_migrate import Migrate
 from sys import argv
 
+from blueprints.usuario import usuario_blueprint
 from database.database import db
 from database.populate import *
 from models.estado import Estado
@@ -12,6 +14,7 @@ from models.loja_fruta import LojaFruta
 from models.usuario import Usuario
 from models.notificacao import Notificacao
 from models.usuario_notificacao import UsuarioNotificacao
+from models.tipo_usuario import TipoUsuario
 
 app = Flask(__name__)
 
@@ -22,13 +25,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = conexao
 db.init_app(app)
 migrate = Migrate(app, db)
 
+app.register_blueprint(usuario_blueprint)
+
 if 'populate' in argv:
     db_load_estado_data(app, db)
     db_load_fruta_data(app, db)
+    db_load_tipo_usuario(app, db)
 
 @app.route('/', methods=['GET'])
 def hello():
     return 'Hello, world!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
